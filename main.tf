bb38895f034e63179caf163aea3b6303e93990ea
John Vinh <john.michael.vinh2@gmail.com>
2025-02-20 12:27:28 -0500

    Define resources

--- main.tf
+++ main.tf
@@ -24,3 +24,114 @@ provider "azurerm" {
 provider "cloudinit" {
   # Configuration options
 }
+
+resource "azurerm_resource_group" "resourcegroup" {
+  name = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name = "${var.labelPrefix}-A05-IP"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  location = azurerm_resource_group.resourcegroup.location
+  allocation_method = "Static"
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name = "${var.labelPrefix}-A05-VN"
+  location = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "${var.labelPrefix}-A05-SN"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "security_group" {
+  name = "${var.labelPrefix}-A05-SG"
+  location = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "${var.labelPrefix}-A05-NIC"
+  location = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  ip_configuration {
+    name                          = "${var.labelPrefix}-A05-IPConfig"
+    subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+}
+
+data "cloudinit_config" "init" {
+  part {
+    content = file("init.sh")
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name = "${var.labelPrefix}-A05-VM"
+  location = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  size = "Standard_B1s"
+  admin_username = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  admin_ssh_key {
+    username = var.admin_username
+    public_key = file("C:\\Users\\whatever\\.ssh\\id_rsa.pub")
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.init.rendered
+}
+
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.resourcegroup.name
+}
